b28fc3b4e7394ebefd40a84bd6281593a7cbd355
Jeff Yang <jho.yang96@gmail.com>
2021-12-01 22:29:00 -0600

    style changes to blog

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,19 +2,19 @@ import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
 export const Footer = styled.footer`
-  ${tw`py-4 bg-gray-100`};
+  ${tw`py-4 text-white bg-indigo-900`};
 `;
 
 export const Links = styled.div`
   ${tw`flex items-center justify-center w-full`};
 
   a {
-    ${tw`mx-2 text-indigo-900 hover:text-indigo-600`};
+    ${tw`mx-2 text-white hover:text-indigo-600`};
   }
 `;
 
 export const Link = styled.a`
-  ${tw`mx-2 text-indigo-900 hover:text-indigo-600`};
+  ${tw`mx-2 text-white hover:text-indigo-600`};
 `;
 
 export const Copyright = styled.div`
--- src/components/Header/MainNav/styles.ts
+++ src/components/Header/MainNav/styles.ts
@@ -8,20 +8,20 @@ interface StyledProps {
 }
 
 export const MainNav = styled.nav<StyledProps>`
-  ${tw`sm:flex flex-col sm:flex-row sm:w-auto w-full order-last sm:order-none my-4 sm:my-0 hidden`};
+  ${tw`flex-col order-last hidden w-full my-4 sm:flex sm:flex-row sm:w-auto sm:order-none sm:my-0`};
   ${({ open }) => open && tw`flex`};
 `;
 
 export const MainNavItem = motion.custom(styled(Link)`
-  ${tw`relative text-indigo-900 border-b border-transparent hover:text-indigo-900 ml-0 sm:ml-8 mt-3 sm:mt-0`};
+  ${tw`relative mt-3 ml-0 text-indigo-900 border-b border-transparent hover:text-indigo-900 sm:ml-8 sm:mt-0`};
   width: max-content;
 
   &.active {
-    ${tw`border-teal-400`};
+    ${tw`border-indigo-400`};
   }
 
   &:before {
-    ${tw`absolute w-full bg-teal-400 h-px left-0 invisible`};
+    ${tw`absolute left-0 invisible w-full h-px bg-indigo-400`};
     content: '';
     bottom: -1px;
     transform: scaleX(0);
@@ -35,11 +35,11 @@ export const MainNavItem = motion.custom(styled(Link)`
 `);
 
 export const ToogleMainNav = styled.button<StyledProps>`
-  ${tw`flex flex-col items-end justify-center cursor-pointer w-6 h-5 sm:hidden`};
+  ${tw`flex flex-col items-end justify-center w-6 h-5 cursor-pointer sm:hidden`};
   outline: none !important;
 
   span {
-    ${tw`bg-indigo-500 inline-block w-6 h-px`};
+    ${tw`inline-block w-6 h-px bg-indigo-500`};
     transition: 0.2s;
 
     &:first-child {
@@ -53,7 +53,7 @@ export const ToogleMainNav = styled.button<StyledProps>`
     }
 
     &:nth-child(2) {
-      ${tw`bg-teal-400 inline-block w-8 h-px`};
+      ${tw`inline-block w-8 h-px bg-indigo-400`};
 
       ${({ open }) => (open ? tw`opacity-0` : tw`opacity-1`)};
       transform: ${({ open }) => (open ? 'translate(20px)' : 'none')};
